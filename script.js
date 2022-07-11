043c7c9ea2cb82b170a12dce9178122641ed394d
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-11 20:32:49 -0300

    Requisito 9 Concluido

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function dblCl() {
+  const list = document.getElementsByTagName('li');
+  for (let index = 0; index < list.length; index += 1) {
+    list[index].addEventListener('dblclick', function () {
+      list[index].classList.toggle('completed');
+    });
+  }
+}
+
 function adicionarTarefa() {
   const btnAdicionar = document.getElementById('criar-tarefa');
   const text = document.getElementById('texto-tarefa');
@@ -6,9 +15,11 @@ function adicionarTarefa() {
   btnAdicionar.addEventListener('click', function () {
     const createLi = document.createElement('li');
     createLi.innerText = text.value;
+    dblCl();
     list.appendChild(createLi);
     text.value = '';
     clickTarefas();
+    dblCl();
   });
 }
 
@@ -20,11 +31,11 @@ function clickTarefas() {
 
   for (let index = 0; index < list.length; index += 1) {
     list[index].addEventListener('click', function (event) {
-      event.target.className = 'colorB';
+      event.target.classList.add('colorB');
       if (selected.length > 1) {
         selected[0].classList.remove('colorB');
       }
-      event.target.className = 'colorB';
+      event.target.classList.add('colorB');
       if (selected.length > 1) {
         selected[1].classList.remove('colorB');
       }
